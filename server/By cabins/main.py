c18845e4cbc29bf14dfc2f326b5aaaf84fd02e62
Dany <86149558+QBoff@users.noreply.github.com>
2023-03-26 20:56:33 +0300

    sixth task

---
+++ server/By cabins/main.py
@@ -0,0 +1,27 @@
+from flask import Flask, redirect, render_template
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@app.route('/distribution')
+def distribution():
+    list_of_astronautes = [("Ридди Скот", "капитан корабля"),
+                           ("Энди Уори", ""),
+                           ("Марк Уотни", ""),
+                           ("Венката Капур", ""),
+                           ("Тедди Сандерс", "")]
+    
+    return render_template("distribution.html",
+                           list_of_astronautes=enumerate(list_of_astronautes), title="По каютам!")
+
+
+
+
+if __name__ == '__main__':
+    app.run()
